f8d3ede73fd965410c84ff47ae6c1718d842e30c
dharmayudhaputu <dharmayudhaputu@gmail.com>
2024-01-31 08:57:52 +0800

    push 12

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World Yudha" }');
+    res.send('{ "response": "Hello World Yudha Tes" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
